e42dea29a67b0879510e05ce779f188ab407a030
Cesar Baez <baezcesar329@gmail.com>
2023-05-19 11:55:49 -0400

    Cambio en la estructura del proyecto

--- front_end/App.js
+++ front_end/App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { getTasks } from './api';
+import { getTasks } from './src/api';
 import { useEffect } from 'react';
 
 export default function App() {
